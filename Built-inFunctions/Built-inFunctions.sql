e36db0fe256180fbc7fc4f44493206f98da13528
Sevgin Mustafa <71196997+sevginmustafa@users.noreply.github.com>
2021-05-14 10:31:42 +0300

    Added changes

--- Built-inFunctions/Built-inFunctions.sql
+++ Built-inFunctions/Built-inFunctions.sql
@@ -2,6 +2,7 @@
 SELECT FirstName, LastName FROM Employees
 WHERE FirstName LIKE 'SA%'
 
+
 --Problem 2. Find Names of All employees by Last Name
 SELECT FirstName, LastName FROM Employees
 WHERE LastName LIKE '%ei%'
